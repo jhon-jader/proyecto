68f0b80a7edd488e8174167ce69e9768b569f01e
Jhon Jader Rojas Rangel <jhonjaderrojas27@gmail.com>
2022-07-18 18:07:45 -0500

    modificacion de (Content-Security-Policy) en la cabecera

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $_SESSION["token"] = $token;
 <html lang="en">
 <head>
     <meta http-equiv="Content-Security-Policy"
-      content="default-src 'self'; frame-src 'self';worker-src 'self' ">
+      content="default-src 'self';worker-src 'self'; form-action 'self' ">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
