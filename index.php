07b4f24321aeba4b84012fef49c44689f36da8dd
Jhon Jader Rojas Rangel <jhonjaderrojas27@gmail.com>
2022-10-02 13:15:09 -0500

    modificacion del body

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ $_SESSION["token"] = $token;
     <title>Sistema de Notas</title>
 </head>
 <body>
-<h2>Identificar si se hacen los cambios</h2>
-    <h1>Inicio de Sesión janajhs</h1>
+    <h1>Inicio de Sesión</h1>
     <form method="POST" action="Usuarios\Controladores\Login.php">
         Usuarios <br>
         <input type="hidden" name="token" value="<?php echo $token; ?>"/>
